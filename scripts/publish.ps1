d9f82176586183548285892a807c293e1c4454c6
Anthony Martin <38542602+anthony-c-martin@users.noreply.github.com>
2025-08-04 03:27:12 -0400

    Fix casing

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -17,14 +17,14 @@ $root="$PSScriptRoot/.."
 $extName="bicep-ext-github"
 
 # build various flavors
-ExecSafe { dotnet publish --configuration release $root -r osx-arm64 }
-ExecSafe { dotnet publish --configuration release $root -r linux-x64 }
-ExecSafe { dotnet publish --configuration release $root -r win-x64 }
+ExecSafe { dotnet publish --configuration Release $root -r osx-arm64 }
+ExecSafe { dotnet publish --configuration Release $root -r linux-x64 }
+ExecSafe { dotnet publish --configuration Release $root -r win-x64 }
 
 # publish to the registry
 ExecSafe { ~/.azure/bin/bicep publish-extension `
-  --bin-osx-arm64 "$root/src/bin/release/osx-arm64/publish/$extName" `
-  --bin-linux-x64 "$root/src/bin/release/linux-x64/publish/$extName" `
-  --bin-win-x64 "$root/src/bin/release/win-x64/publish/$extName.exe" `
+  --bin-osx-arm64 "$root/src/bin/Release/osx-arm64/publish/$extName" `
+  --bin-linux-x64 "$root/src/bin/Release/linux-x64/publish/$extName" `
+  --bin-win-x64 "$root/src/bin/Release/win-x64/publish/$extName.exe" `
   --target "$target" `
   --force }
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,14 +8,14 @@ root="$(dirname ${BASH_SOURCE[0]})/.."
 ext_name="bicep-ext-github"
 
 # build various flavors
-dotnet publish --configuration release $root -r osx-arm64
-dotnet publish --configuration release $root -r linux-x64
-dotnet publish --configuration release $root -r win-x64
+dotnet publish --configuration Release $root -r osx-arm64
+dotnet publish --configuration Release $root -r linux-x64
+dotnet publish --configuration Release $root -r win-x64
 
 # publish to the registry
 ~/.azure/bin/bicep publish-extension \
-  --bin-osx-arm64 "$root/src/bin/release/osx-arm64/publish/$ext_name" \
-  --bin-linux-x64 "$root/src/bin/release/linux-x64/publish/$ext_name" \
-  --bin-win-x64 "$root/src/bin/release/win-x64/publish/$ext_name.exe" \
+  --bin-osx-arm64 "$root/src/bin/Release/osx-arm64/publish/$ext_name" \
+  --bin-linux-x64 "$root/src/bin/Release/linux-x64/publish/$ext_name" \
+  --bin-win-x64 "$root/src/bin/Release/win-x64/publish/$ext_name.exe" \
   --target "$target" \
   --force
